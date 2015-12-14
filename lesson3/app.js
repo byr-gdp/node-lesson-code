879a36fec196afca81cee15fa4c4fdf9979a4e11
byr-gdp <anthozoan77@gmail.com>
2015-12-14 16:07:58 +0800

    to heroku

--- lesson3/app.js
+++ lesson3/app.js
@@ -34,6 +34,6 @@ app.get("/", function(req, res) {
 	})
 })
 
-app.listen(3000, function() {
-	console.log("listen at port 3000");
+app.listen(process.env.PORT || 5000, function() {
+	console.log("listen at port 5000");
 })
